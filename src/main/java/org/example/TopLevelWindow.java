f2381a66e6f4b216583d09725b64ae9bc4105a00
JuanSV22 <j.soto26@ufromail.cl>
2023-05-04 08:53:28 -0400

    Initial commit

---
+++ src/main/java/org/example/TopLevelWindow.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import javax.swing.*;
+
+public class TopLevelWindow {
+    private static void createWindow(){
+        JFrame frame = new JFrame("Hiiii");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JLabel textLabel = new JLabel("Hi how you doiiin.",SwingConstants.CENTER);
+        
+    }
+}
